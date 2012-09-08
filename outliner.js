4b0430aaf89f4fe09d0fcca2e5979141c4cdca7d
Scott Cheng <scott.cheng.x@gmail.com>
2012-09-08 13:14:24 +0800

    bug fixes, removed invisible headings

--- outliner.js
+++ outliner.js
@@ -1,11 +1,11 @@
 var shrinkBody = function() {
   $('body').css({
-    marginLeft: 285  // 280?
+    marginLeft: 280
   });
 };
 
 var expandBody = function() {
-  $body.css({
+  $('body').css({
     marginLeft: 0
   });
 };
@@ -17,7 +17,7 @@ var showSidebar = function() {
 };
 
 var hideSidebar = function() {
-  $sidebar.css({
+  $('#outliner-sidebar').css({
     left: -280
   });
 };
@@ -57,6 +57,16 @@ $(function() {
     .html(HTML5Outline(document.body).asHTML(true))
     .appendTo($contentWrapper);
 
+  // find invisible headings
+  var $anchors = $content.find('a');
+  for (var i = 0; i < $anchors.length; i++) {
+    var $anchor = $($anchors[i]);
+    var href = $anchor.attr('href');
+    if (href && !$(href).is(':visible')) {
+      $anchor.addClass('outliner-invisible');
+    }
+  }
+
   var $body = $('body');
   var top = window.parseInt($body.css('padding-top'))
           + window.parseInt($body.css('margin-top'));
@@ -65,4 +75,5 @@ $(function() {
     .css('top', top)
     .appendTo($body);
   showSidebar();
+  // window.setTimeout(showSidebar, 250);
 });
